34711a4c8167736a9cb927ebf77fd669d137e4b8
Angela Cearnau <cearnau.angela@gmail.com>
2020-02-26 17:37:14 +0100

    Addded acceptance tests

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,7 +1,79 @@
 import { browser, by, element } from 'protractor';
+import { SSL_OP_EPHEMERAL_RSA } from 'constants';
 
 export class AppPage {
   navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+    return browser.get(browser.baseUrl) as Promise<any>;       
   }
+
+
+  checkFirstItem() {
+    var li = element(by.xpath('//ul/li'));
+    expect(li.getText()).toMatch('Item 1 - Level');
+    console.log("first item" + li.getText());
+  }
+
+
+  async searchTerm(term){
+    await element(by.xpath("//input[@placeholder='Search']")).clear().then(function(){
+       element(by.xpath("//input[@placeholder='Search']")).sendKeys(term);
+       //browser.pause(2000);
+    })
+   
+  }
+
+  checkItemList() {    
+    element.all(by.xpath('//ul/li')).count().then(function(text){
+        console.log("Items on page: " + text)
+    })   
+  }
+
+  async getItemName(index) {
+    var li = element(by.css('ul')).element(by.css("li:nth-child("+ index + ")")).getText();
+    return li;    
+  }
+
+  async clickItemByName(elemName) {    
+      await element.all(by.css('ul li')).each(async elem=> {
+        await elem.getText().then(text=>  {
+        if ( text.includes(elemName))  {         
+         elem.element(by.css("Button")).click();
+        }
+      })    
+    })
+  }
+
+
+  async getItemInfo(index) {        
+    var info = await element(by.css('div.item')).element(by.css("div:nth-child("+ index + ")")).getText()
+    return info      
+  }
+
+
+  async countItemList() {  
+    const newcount = await element.all(by.xpath('//ul/li')).count()
+    return newcount
+  }
+
+ 
+  async getButtonName(index) {        
+    var buttonText = element(by.xpath('//ul/li['+ index + ']/button')).getText()
+    return buttonText      
+  }
+
+
+  async checkLevel0(){
+    
+    const allItems = await element.all(by.xpath('//ul/li[@class=""]')).count()
+    return allItems;
+
+  }
+
+  async checkItemLevel(level){
+    
+    const allItems = await element.all(by.css('ul li' + level)).count()
+    return allItems;
+
+  }  
+
 }
--- e2e/src/app.spec.ts
+++ e2e/src/app.spec.ts
@@ -1,14 +1,120 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, element } from 'protractor';
 
 describe('cobiro test', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
-  });
-
-  it('should go to the webpage', () => {
     page.navigateTo();
   });
-});
+
+  /*
+  it('should go to the webpage', () => {
+    //page.navigateTo();
+    page.checkItemList();    
+  });
+  */
+
+
+  it('all menu items should have a button named "Button"', () => {
+    for(var i=1; i<=8; i++){
+      expect(page.getButtonName(i)).toEqual("Button");   
+    }
+   
+  });
+
+
+  it('check tree structure exists', () => {
+    expect(page.checkItemLevel(".indent-1")).toBeGreaterThan(1);  
+    expect(page.checkItemLevel(".indent-2")).toBeGreaterThan(1);  
+    expect(page.checkItemLevel("")).toBeGreaterThan(1); 
+    
+  });
+
+  
+  it('total number of items should be 8', () => {
+    page.navigateTo();
+    expect(page.countItemList()).toBe(8);
+  });
+
+  it('should display correct names for items', () => {
+    page.navigateTo();
+    page.getItemName(1).then(function(text){
+      var newtext = text.replace("Button","").trim();
+      expect(newtext).toEqual("Item 1");  // according to the design spec
+      })     
+  });
+
+  
+
+  it('should filter items correctly', () => {
+    const testCases = [
+      { term: 'Item 44', total: 0, level0: 0, level1: 0, level2: 0},      // no match
+      { term: 'Item 2 - Level 1', total: 8, level0: 3, level1: 2, level2: 3},    // full title - WILL FAIL
+      { term: 'Item 1', total: 3, level0: 1, level1: 1, level2: 1},
+      { term: 'Item 4', total: 1, level0: 1, level1: 0, level2: 0},      
+      { term: 'Item 5', total: 4, level0: 1, level1: 1, level2: 2},      
+      { term: 'Item 2', total: 2, level0: 0, level1: 1, level2: 1},      
+      { term: 'Item 6', total: 3, level0: 0, level1: 1, level2: 2},      
+      { term: 'Item 3', total: 1, level0: 0, level1: 0, level2: 1},      
+      { term: 'Item 7', total: 1, level0: 0, level1: 0, level2: 1},      
+      { term: 'Item 8', total: 1, level0: 0, level1: 0, level2: 1},    
+      { term: 'tem 4', total: 1, level0: 1, level1: 0, level2: 0},   //partial match
+      { term: 'Item', total: 8, level0: 3, level1: 2, level2: 3},    //match all  items   
+     
+    ];
+  
+    testCases.forEach(async test => {
+
+        page.searchTerm(test.term)
+        expect(page.countItemList()).toEqual(test.total);              //total number of items 
+        expect(page.checkLevel0()).toEqual(test.level0);               //level 0 items
+        expect(page.checkItemLevel(".indent-1")).toEqual(test.level1); //level 1 items 
+        expect(page.checkItemLevel(".indent-2")).toEqual(test.level2); //level 2 items
+     
+    });
+  });
+
+
+
+  it('should show correct infomation on each item when clicking Button', () => {
+
+    const testCases = [
+      { item: 'Item 3', info1: 'Item ID: 4', info2: 'Item Title: Item 4', info3: 'Item Parent ID:'},      // actual result - BUG
+      { item: 'Item 1', info1: 'Item ID: 2', info2: 'Item Title: Item 2', info3: 'Item Parent ID: 1'},    // actual result - BUG
+      { item: 'Item 1', info1: 'Item ID: 1', info2: 'Item Title: Item 1', info3: 'Item Parent ID:'},      // WILL FAIL
+      { item: 'Item 2', info1: 'Item ID: 2', info2: 'Item Title: Item 2', info3: 'Item Parent ID: 1'},    // WILL FAIL
+      { item: 'Item 3', info1: 'Item ID: 3', info2: 'Item Title: Item 3', info3: 'Item Parent ID: 2'},    // WILL FAIL
+      { item: 'Item 4', info1: 'Item ID: 4', info2: 'Item Title: Item 4', info3: 'Item Parent ID:'},      // WILL FAIL
+      { item: 'Item 5', info1: 'Item ID: 5', info2: 'Item Title: Item 5', info3: 'Item Parent ID:'},      // WILL FAIL
+      { item: 'Item 6', info1: 'Item ID: 6', info2: 'Item Title: Item 6', info3: 'Item Parent ID: 5'},    // WILL FAIL
+      { item: 'Item 7', info1: 'Item ID: 7', info2: 'Item Title: Item 7', info3: 'Item Parent ID: 6'},    // WILL FAIL
+      { item: 'Item 8', info1: 'Item ID: 8', info2: 'Item Title: Item 8', info3: 'Item Parent ID: 6'},    // WILL FAIL
+        
+    ];
+
+    testCases.forEach(async test => {
+      page.navigateTo();
+      page.clickItemByName(test.item);
+      expect(page.getItemInfo(1)).toEqual(test.info1);
+      expect(page.getItemInfo(2)).toEqual(test.info2);    
+      expect(page.getItemInfo(3)).toEqual(test.info3);
+    })
+
+
+  }); 
+
+
+  it('Should show information on an item from filtered list when clicking button', () => {
+    page.navigateTo();
+    page.searchTerm('Item 2');
+    page.clickItemByName('Item 3');
+
+    expect(page.getItemInfo(1)).toEqual('Item ID: 4'); //BUG
+    //expect(page.getItemInfo(1)).toEqual('Item ID: 3');    //EXPECTED, WILL FAIL
+    
+  });
+  
+
+})
